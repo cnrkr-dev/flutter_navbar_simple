84c95caa8e3ef71fe2f62456d8dcc64df30ffc8c
cnrkr-dev <cnrkr.dev@gmail.com>
2024-09-06 23:31:29 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainApptate();
+}
+
+class _MainApptate extends State<MainApp> {
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('NavigationBar Sample'),
+        ),
+        bottomNavigationBar: NavigationBar(
+          destinations: const [
+            NavigationDestination(icon: Icon(Icons.home), label: "tab 1"),
+            NavigationDestination(icon: Icon(Icons.person), label: "tab 2"),
+            NavigationDestination(icon: Icon(Icons.settings), label: "tab 3")
+          ],
+          onDestinationSelected: (value) {
+            setState(() {
+              currentPageIndex = value;
+            });
+          },
+        ),
+        body: <Widget>[
+          const Card(
+            color: Colors.blue,
+            shadowColor: Colors.transparent,
+            margin: EdgeInsets.all(8.0),
+            child: SizedBox.expand(
+              child: Center(
+                child: Text('Tab 1',
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ),
+            ),
+          ),
+          const Card(
+            color: Color.fromARGB(255, 176, 231, 80),
+            shadowColor: Colors.transparent,
+            margin: EdgeInsets.all(8.0),
+            child: SizedBox.expand(
+              child: Center(
+                child: Text('Tab 2',
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ),
+            ),
+          ),
+          const Card(
+            shadowColor: Colors.transparent,
+            margin: EdgeInsets.all(8.0),
+            child: SizedBox.expand(
+              child: Center(
+                child: Text('Tab 3',
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ),
+            ),
+          ),
+        ][currentPageIndex],
+      ),
+    );
+  }
+}
