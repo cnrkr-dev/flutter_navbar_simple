654d745ff1db57cd5d12fd6dfdd1a87ba3b3f28b
“cnrkr” <cnrkr.dev@gmail.com>
2024-09-07 00:06:48 +0300

    s2

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class _MainApptate extends State<MainApp> {
         bottomNavigationBar: NavigationBar(
           destinations: const [
             NavigationDestination(icon: Icon(Icons.home), label: "page 1"),
-            NavigationDestination(icon: Icon(Icons.person), label: "tab 2"),
+            NavigationDestination(icon: Icon(Icons.person), label: "page 2"),
             NavigationDestination(icon: Icon(Icons.settings), label: "tab 3")
           ],
           onDestinationSelected: (value) {
